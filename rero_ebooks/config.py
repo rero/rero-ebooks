dddb5384450bcf2b30bb327d70cc86b805f562b2
Peter Weber <Peter.Weber@rero.ch>
2018-08-14 11:25:52 +0200

    feat: oai harvesters config
    Signed-off-by: Peter Weber <Peter.Weber@rero.ch>
    Co-authored-by: Bertrand Zuchuat <Bertrand.Zuchuat@rero.ch>

--- rero_ebooks/config.py
+++ rero_ebooks/config.py
@@ -16,7 +16,7 @@ You overwrite and set instance-specific configuration by either:
 from __future__ import absolute_import, print_function
 
 from datetime import timedelta
-
+from invenio_oaiharvester.signals import oaiharvest_finished
 
 def _(x):
     """Identity function used to trigger string extraction."""
@@ -165,3 +165,12 @@ OAISERVER_RECORD_INDEX = 'ebooks'
 
 #: Switches off incept of redirects by Flask-DebugToolbar.
 DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+# OAI_HARVESTER_TASKS
+# CELERYBEAT_SCHEDULE = {}
+# for harvest in OAI_HARVESTERS:
+#     task = {
+#         'task': 'rero_ebooks.tasks.harvest_{0}'.format(harvest['id']),
+#         'schedule': timedelta(minutes=harvest.get('timedelta', 60*24)),
+#     }
+#     CELERYBEAT_SCHEDULE[harvest['id']] = task
